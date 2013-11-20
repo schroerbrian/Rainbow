a5e4c95ba8cadd80e0a95e5c63ebe94f22d6cedc
Brian Schroer <schroer.brian@gmail.com>
2013-11-20 13:57:41 -0800

    change number to integer on plu model. oops.

--- db/migrate/20131120205748_add_number_to_plu_code.rb
+++ db/migrate/20131120205748_add_number_to_plu_code.rb
@@ -2,6 +2,6 @@ class AddNumberToPluCode < ActiveRecord::Migration
   def change
     add_column :plu_codes, :number, :integer
     add_column :plu_codes, :description, :string
-    add_column :plu_codes, :flag, :number
+    add_column :plu_codes, :flag, :integer
   end
 end

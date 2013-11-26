53b49c840fe0d7a865aca180e7aa718394b8df33
Jesse Sanford <jesse@berkeley.edu>
2013-11-25 19:19:23 -0800

    remove reference to Resque::Mailer

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -6,10 +6,6 @@ rails_env = Rails.env || 'development'
 resque_config = YAML.load_file(File.join(rails_root,"/config/resque.yml"))
 Resque.redis = resque_config[rails_env]
 
-Resque::Mailer.default_queue_name = 'scanner'
-Resque::Mailer.excluded_environments = [:test]
-
-
 Resque.logger = Logger.new('log/resque.log', 10, 1024000)
 
 Resque.inline = Rails.env.test?
